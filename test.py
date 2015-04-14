b65bb9ffb12dabc39e2c300b1f649f1b006a900f
Ali <alisangari>
2015-04-14 19:10:23 -0400

    blob

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ db = understand.open(dbfile)
 for file in (db.lookup(".cpp","File")):
 
 	// To make sure only classes are being picked up
-	if((ent.kindname() == "Class") or (ent.kindname() == "Public Class") or (ent.kindname() == "Unknown Class") or (ent.kindname() == "Private Class")):
+	if(ent.kindname() == "Class"):
 
 	// To get CBO/LCOM for a class. -1 is used as error code.
 	metric = file.metric(("CountClassCoupled",))
